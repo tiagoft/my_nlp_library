7d83c4346c638112340c8c2f5c13255eaf6fed8f
Tiago Tavares <tiagoft@gmail.com>
2024-10-14 12:00:03 -0300

    Testing also uses GPU

--- my_nlp_library/training.py
+++ my_nlp_library/training.py
@@ -38,20 +38,33 @@ def train_binary_model(model, dataset, n_epochs=100, batch_size=64, lr=1e-3):
     return model, losses
 
 def test_binary_model(model, dataset, batch_size=64):
+
+    if torch.cuda.is_available():
+        use_cuda = True
+    else:
+        use_cuda = False
+
     dataloader_test = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     model.eval()  # Set the model to evaluation mode
+    if use_cuda == True:
+        model = model.cuda()
     with torch.no_grad():
         acc = 0
         n_tests = 0
-        for X_test_vect, y_test_vect in dataloader_test:
+        for batch in dataloader_test:
+            if use_cuda == True:
+                batch = [item.cuda() for item in batch]
+            X_test_vect, y_test_vect = batch
             output = model(X_test_vect)
             output_probs = torch.sigmoid(output)
             predictions = (output_probs > 0.5).int()
 
             # get batch accuracy
-            accuracy = accuracy_score(y_test_vect, predictions)
+            accuracy = accuracy_score(y_test_vect.detach().cpu().numpy(), predictions.detach().cpu().numpy())
             acc += accuracy * len(y_test_vect)
             n_tests += len(y_test_vect)
     # Calculate accuracy
     accuracy = acc / n_tests
+    if use_cuda == True:
+        model = model.cpu()
     return accuracy
